eedf5d86389a8de92232b53e64fa06c8333c8941
Beata Karolczak <karolczak.beata@gmail.com>
2018-09-16 20:08:21 +0200

    Update webpack and hot module

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+//var UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+var OptimizeJsPlugin = require('optimize-js-plugin');
+var env = process.env.NODE_ENV || 'development';
+
+const plugins = [
+	new HtmlWebpackPlugin({
+        template: 'client/index.html',
+        filename: 'index.html',
+        inject: 'body',
+    })
+];
+
+console.log('NODE_ENV:', env);
+
+if (env === 'production') {
+	plugins.push(
+    	//new webpack.optimize.UglifyJsPlugin(),
+    	new OptimizeJsPlugin({
+      	sourceMap: false
+    	})
+  	);
+}
+
+
+module.exports = {
+	entry: [
+		'react-hot-loader/patch',
+		'./client/index.js'
+	],
+	output: {
+		path: path.resolve(__dirname, 'public'),
+		filename: 'app.bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: "babel-loader"
+			},
+			{
+				test: /\.css$/,
+				use: [
+					{loader: 'style-loader'},
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true
+						}
+					}
+				]
+			}
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: 'public/index.html',
+			filename: 'index.html',
+			inject: 'body'
+			}),
+		//new webpack.optimize.UglifyJsPlugin(),
+		new OptimizeJsPlugin({
+			sourceMap: false
+		})
+	],
+	devServer: {
+		proxy: {
+			'/socket.io' : {
+				target: 'http://localhost:3000',
+				ws: true
+			}
+		}
+	}
+};
+
